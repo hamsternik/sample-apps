0b686e51d00e2243ab07581284755881152c0f36
Nikita Khomitsevich <hamsternik9@gmail.com>
2017-03-19 19:41:08 +0200

    Implemented  design in storyboard

---
+++ gpiocontrol/source/objc/GPIOControlDemo/Sources/CreateGroupViewController.m
@@ -0,0 +1,21 @@
+//
+//  CreateGroupViewController.m
+//  GPIOControlDemo
+//
+//  Created by Nikita Khomitsevich on 3/19/17.
+//  Copyright © 2017 cybervisiontech. All rights reserved.
+//
+
+#import "CreateGroupViewController.h"
+
+@interface CreateGroupViewController ()
+
+@end
+
+@implementation CreateGroupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
--- gpiocontrol/source/objc/GPIOControlDemo/Sources/GroupAndControllerAddingViewController.m
+++ gpiocontrol/source/objc/GPIOControlDemo/Sources/GroupAndControllerAddingViewController.m
@@ -8,6 +8,7 @@
 
 #import "GroupAndControllerAddingViewController.h"
 #import "ControllerAddingViewController.h"
+#import "CreateGroupViewController.h"
 
 @interface GroupAndControllerAddingViewController ()
 
@@ -20,6 +21,14 @@
 }
 
 #pragma mark - IBActions
+
+- (IBAction)presentAddGroupDialog:(id)sender {
+    CreateGroupViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"CreateGroupViewControllerID"];
+    vc.providesPresentationContextTransitionStyle = YES;
+    vc.definesPresentationContext = YES;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
 
 - (IBAction)presentAddControllerDialog:(id)sender {
     ControllerAddingViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ControllerAddingViewControllerID"];
